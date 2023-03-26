fb2ec325ea7691db29588475eea2f52c74704044
ErCrozzUni <128230520+ErCrozzUni@users.noreply.github.com>
2023-03-26 16:07:13 +0200

    yes, the tests
    yes yes

---
+++ test/TestLabirinto.java
@@ -0,0 +1,24 @@
+package it.diadia.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestLabirinto {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		
+	}
+
+}
