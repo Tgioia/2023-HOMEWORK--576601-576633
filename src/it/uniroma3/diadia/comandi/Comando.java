82979759fb88cf129fdca4b7048c0c08fe6a09d7
Crozz <128230520+ErCrozzUni@users.noreply.github.com>
2023-06-05 11:34:34 +0200

    Delete src/it/uniroma3/diadia directory

--- src/it/uniroma3/diadia/comandi/Comando.java
+++
@@ -1,9 +0,0 @@
-package it.uniroma3.diadia.comandi;
-import it.uniroma3.diadia.Partita;
-
-public interface Comando {
-	public void esegui(Partita partita);
-	public String getNome();
-	public String getParametro();
-	public void setParametro(String parametro);
-}
--- src/it/uniroma3/diadia/comandi/FabbricaDiComandi.java
+++
@@ -1,5 +0,0 @@
-package it.uniroma3.diadia.comandi;
-
-public interface FabbricaDiComandi {
-	public Comando costruisciComando(String istruzione);
-}
